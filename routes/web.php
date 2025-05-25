1741202fcfc4ed6885f92122d3e4a953583ce4a0
Mitsuya-Haruki <bouipenspi@gmail.com>
2025-05-25 05:40:15 +0000

    learned about pagination

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/posts', [PostController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    return view('posts.index');
 });
